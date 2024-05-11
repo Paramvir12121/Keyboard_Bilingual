a2cbfc2527cdfe2ac1b2a6dbe676d50c66ea35f4
Paramvir Singh <paramvir12121@gmail.com>
2024-05-11 09:46:34 +0000

    added some frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import About from './components/About'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
+import Settings from './components/windows/Settings'
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   )
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Notes from './windows/Notes'
-import Tasks from './windows/Tasks'
 
 const Home = () => {
   return (
     <>
     Home 
-      <Tasks />
+
     </>
   )
 }
