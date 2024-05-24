8c80fcc53b83a43d194aea41e5bd624bdda52b03
paramvir12121 <paramvir12121@gmail.com>
2024-05-24 02:12:14 -0400

    working id jwtdocode

--- frontend/src/components/tests/ProtectedData.jsx
+++ frontend/src/components/tests/ProtectedData.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import LogoutButton from '../auth/LogoutButton';
 import baseApi from '../Api/BaseApi';
+import { jwtDecode } from "jwt-decode";
 
 
 const ProtectedData = () => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [userName, setUserName] = useState('');
+    const [email,setEmail] = useState('')
+    
 
     useEffect(() => {
         const fetchProtectedData = async () => {
@@ -18,11 +20,13 @@ const ProtectedData = () => {
                     return;
                 }
                 // Decode the token to get user information
-                // const id_token = localStorage.getItem('id_token');
-                // const decodedToken = jwtDecode(id_token);
-                // console.log(decodedToken)
-                //const userName = decodedToken.name || decodedToken.email || 'User'; // Adjust according to your token payload
-                // setUserName(userName);
+                const id_token = localStorage.getItem('id_token');
+                const decodedToken = jwtDecode(id_token);
+                console.log("Decoded Token: ",decodedToken.username)
+                setUserName(decodedToken.preferred_username)
+                setEmail(decodedToken.email)
+                // const userName = decodedToken.username || 'User'; // Adjust according to your token payload
+                // ;
 
 
                 const response = await baseApi.post('/auth/protected', {}, {
@@ -32,6 +36,7 @@ const ProtectedData = () => {
                 });
 
                 setData(response.data);
+                console.log("data: ",data)
             } catch (error) {
                 if (error.response && error.response.status === 401) {
                     setError('Unauthorized. Please log in again.');
@@ -44,15 +49,23 @@ const ProtectedData = () => {
         fetchProtectedData();
     }, []);
 
+    // const fetchUserName = () =>{
+    //     // Decode the token to get user information
+    //     const access_token = localStorage.getItem('access_token');
+    //     const decodedToken = jwtDecode(access_token);
+    //     console.log("Decoded Token: ",decodedToken.username)
+    //     username = decodedToken.username.toUpperCase()
+    //     setUserName(username)
+    // }
+
     return (
         <div>
             <h2>Protected Data</h2>
             <LogoutButton />
             {error && <p style={{ color: 'red' }}>{error}</p>}
-            <h2>Welcome</h2>
             {data ? (
                 <div>
-                    <p>{data.message}</p>
+                    <p>{data.message}! Welcome {userName} {email}</p>
                 </div>
             ) : (
                 !error && <p>Loading...</p>
