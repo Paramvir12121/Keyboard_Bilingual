65c92a83533d71e86269844c69a6e625adbc580b
paramvir12121 <paramvir12121@gmail.com>
2024-05-31 07:48:17 -0400

    working custom useAuth hook

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,37 +1,10 @@
 import { useState, useEffect } from 'react';
 import baseApi from './Api/BaseApi';
+import useAuth from './auth/useAuth';
 
 const Home = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userId, setUserId] = useState(null);
-    const [idToken, setIdToken] = useState(null);
-    const [username, setUsername] = useState(null);
-
-    useEffect(() => {
-        const checkLoginStatus = async () => {
-            try {
-                const response = await baseApi.get('/auth/checklogin', {
-                    withCredentials: true // Ensure cookies are sent with the request
-                });
-                console.log("response:", response.data)
-                if (response.data.message === 'User is logged in.') {
-                    setIsLoggedIn(true);
-                    setUserId(response.data.user_id);
-                    setIdToken(response.data.id_token);
-                    setUsername(response.data.username);
-                }
-            } catch (error) {
-                setIsLoggedIn(false);
-                setUserId(null);
-                setIdToken(null);
-                setUsername(null);
-            }
-        };
-        console.log("Username: ",username)
-
-        checkLoginStatus();
-    }, []);
-
+    const { isLoggedIn, userId,idToken, username } = useAuth();
+    
 
     return (
         <>
