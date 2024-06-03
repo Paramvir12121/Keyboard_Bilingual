30a996adbcd88dbb9266bdc808ea62ca84a99f39
Paramvir Singh <paramvir12121@gmail.com>
2024-06-03 09:42:09 +0000

    working client env

--- frontend/src/components/hooks/BaseApi.jsx
+++ frontend/src/components/hooks/BaseApi.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
+console.log(import.meta.env.VITE_API_BASE_URL); 
+
 const baseApi = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
-    //  baseURL: 'https://fantastic-space-halibut-6v5j5pqg7jqfrj5w-5000.app.github.dev/'
-    // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000',
 });
 
 export default baseApi;
