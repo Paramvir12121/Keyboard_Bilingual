3484709f76c1071e0b76c254a192ae13e9098cc4
paramvir12121 <paramvir12121@gmail.com>
2024-06-05 13:35:38 -0400

    added some links

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import baseApi from './hooks/BaseApi';
 import useAuth from './hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/esm/Button';
 
 const Home = () => {
     const { isLoggedIn, userId,idToken, username } = useAuth();
+    const navigate = useNavigate()
     
 
     return (
@@ -11,7 +14,10 @@ const Home = () => {
 
             Home
             {isLoggedIn ? (
+                <> 
                 <p>You are logged in as: {username}</p>
+                <Button variant="link" onClick={()=>navigate('/lessonlist')}>Lessonlist</Button>
+                </>
             ) : (
                 <p>You are not logged in.</p>
             )}
