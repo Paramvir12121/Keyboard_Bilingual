85a3433a859ff50e38da6b984591738d9d131355
Paramvir Singh <paramvir12121@gmail.com>
2024-06-21 03:47:00 +0000

    added dashboard

--- secondend/src/components/common/SuccessMessage.jsx
+++ secondend/src/components/common/SuccessMessage.jsx
@@ -1,7 +1,17 @@
+
+
+import { Navigate } from "react-router-dom";
+
 const SucessMessage = ({ message }) => {
+    handleRedirect = () => {
+        Navigate("/dashboard");
+    }
+   
+
     return (
         message ? <div className="alert alert-success" role="alert">{message}</div> : null
     );
 }
 
 export default SucessMessage;
+
