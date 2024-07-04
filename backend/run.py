a559bb9f48344db5a318e3501d824105283b3ea1
Paramvir Singh <paramvir12121@gmail.com>
2024-07-04 20:54:48 +0000

    backend container working

--- backend/run.py
+++ backend/run.py
@@ -2,9 +2,13 @@ from main import create_app
 from config import DevConfig
 from exts import db
 
+#
+
+app = create_app(DevConfig)
+
+
 
 if __name__ == '__main__':
-    app=create_app(DevConfig)
     with app.app_context():  # Create an application context if not in a request context
         db.create_all()
     app.run(debug=True,host='0.0.0.0', port=5000)
