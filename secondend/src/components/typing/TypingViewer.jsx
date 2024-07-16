df7bd8c9c310925c8c7689e1164457dcb98f35d6
Paramvir Singh <paramvir12121@gmail.com>
2024-07-16 03:27:46 +0000

    Typing Viewer working

--- secondend/src/components/typing/TypingViewer.jsx
+++ secondend/src/components/typing/TypingViewer.jsx
@@ -6,7 +6,7 @@ import { qwertyToColemak } from '../keyboard/Layouts';
 // import Timer from '../timer/Timer';
 
 const TypingViewer = ({words}) => {
-  const [displayText, setDisplayText] = useState('Empty');
+  const [displayText, setDisplayText] = useState('');
   const [cursorIndex, setCursorIndex] = useState(0);
   const [isWrongKey, setIsWrongKey] = useState(false);
   const [isColemak, setIsColemak] = useState(false);
@@ -22,8 +22,9 @@ const TypingViewer = ({words}) => {
       console.error('No words provided');
       return;
     }
+
     let viewText = '';
-    
+
     for (let i = 0; i < 20; i++) {
       viewText += words[Math.floor(Math.random() * words.length)] + ' ';
     }
@@ -37,14 +38,48 @@ const TypingViewer = ({words}) => {
     setIsColemak(keyboardType === 'colemak');
   } , [generateNewText]);
 
-  
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      let pressedKey = event.key.toLowerCase();
+      if (isColemak && qwertyToColemak[pressedKey]) {
+        pressedKey = qwertyToColemak[pressedKey];
+      }
+      setKeysTyped(prev => prev + 1);
+      if (pressedKey === displayText[cursorIndex].toLowerCase() || (pressedKey === ' ' && displayText[cursorIndex] === ' ')) {
+        setIsWrongKey(false);
+        setCursorIndex(prevIndex => {
+          if (prevIndex + 1 >= displayText.length) {
+            generateNewText();
+            return 0;
+          }
+          return prevIndex + 1;
+        });
+      } else {
+        setIsWrongKey(true);
+        setErrorCount(prev => prev + 1);
+      }
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [cursorIndex, displayText, generateNewText, isColemak]);
 
 
   return (
     <>
     <h2>Typing Viewer</h2>
-    <div>{displayText}</div>
-    <div>{words[3]}</div>
+    {/* <div>{displayText}</div>
+    <div>{words[3]}</div> */}
+    {displayText.split('').map((char, index) => (
+              <span 
+                key={index} 
+                className={`character ${index === cursorIndex ? (isWrongKey ? 'cursor wrong' : 'cursor') : ' '}`}
+              >
+                {char}
+                {char === ' ' ? <span>&nbsp;</span> : null}
+              </span>
+            ))}
 
     </>
     
