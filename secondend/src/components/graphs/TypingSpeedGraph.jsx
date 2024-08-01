c6ed56edbef691a4bc37eab9c755b8f5063e8f6b
Paramvir Singh <paramvir12121@gmail.com>
2024-08-01 03:10:35 +0000

    imported new library for graphs

---
+++ secondend/src/components/graphs/TypingSpeedGraph.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LineChart, Line } from 'recharts';
+
+const TypingSpeedGraph = () => {
+    
+
+  return (
+    <div>TypingSpeedGraph</div>
+  )
+}
+
+export default TypingSpeedGraph
---
+++ secondend/src/hooks/getUserStats.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import baseApi from './baseApi';
+
+const getUserStats = () => {
+    const api = baseApi();
+
+    
+
+
+
+  return (
+    <div>getUserStats</div>
+  )
+}
+
+export default getUserStats
