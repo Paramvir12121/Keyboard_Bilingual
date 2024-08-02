e61b7fbeb962f1fe84d95e3de643e62496da05b4
Paramvir Singh <paramvir12121@gmail.com>
2024-08-02 03:26:09 +0000

    working on graphs

--- secondend/src/components/graphs/AccuracyGraph.jsx
+++ secondend/src/components/graphs/AccuracyGraph.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { getTypingSpeed } from '../../hooks/getUserStats';
 
-const TypingSpeedGraph = () => {
+const AccuracyGraph = () => {
   const [typingData, setTypingData] = useState([]);
 
   useEffect(() => {
@@ -19,13 +19,12 @@ const TypingSpeedGraph = () => {
     <ResponsiveContainer width="100%" height={300}>
       <LineChart data={typingData}>
         <XAxis dataKey="name" />
-        <YAxis />
+        <YAxis domain={[75, 100]} />
         <Tooltip />
-        <Line type="monotone" dataKey="score" stroke="#8884d8" strokeWidth={2} />
-        {/* <Line type="monotone" dataKey="accuracy" stroke="#ffffff" strokeWidth={2} /> */}
+        <Line type="monotone" dataKey="accuracy" stroke="#ffffff" strokeWidth={2} />
       </LineChart>
     </ResponsiveContainer>
   );
 }
 
-export default TypingSpeedGraph;
+export default AccuracyGraph;
--- secondend/src/pages/dashboard/Graphs.jsx
+++ secondend/src/pages/dashboard/Graphs.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import TypingSpeedGraph from '../../components/graphs/TypingSpeedGraph'
+import React from 'react';
+import TypingSpeedGraph from '../../components/graphs/TypingSpeedGraph';
+import AccuracyGraph from '../../components/graphs/AccuracyGraph';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Graphs = () => {
   return (
-    <>
-    <div>Graphs
-    <TypingSpeedGraph />
-    </div>
-    </>
-  )
+    
+      <Row>
+        <Col lg={6} className="mb-3">
+          <TypingSpeedGraph />
+        </Col>
+        <Col lg={6} className="mb-3">
+          <AccuracyGraph />
+        </Col>
+      </Row>
+    
+  );
 }
 
-export default Graphs
+export default Graphs;
