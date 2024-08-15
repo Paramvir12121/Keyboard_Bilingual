6f27922debd0ebd8bb0d97098449554cf3207eef
paramvir12121 <paramvir12121@gmail.com>
2024-08-15 02:26:43 -0400

    minor updates

--- frontend/src/hooks/baseApi.jsx
+++ frontend/src/hooks/baseApi.jsx
@@ -1,29 +1,19 @@
 import axios from 'axios'
 
 console.log(import.meta.env.VITE_API_URL)
+console.log(import.meta.env);
+
 
 const baseApi = () => {
     const api = axios.create({
         baseURL: import.meta.env.VITE_API_URL,
-        // withCredentials: true,
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         }
-    })
+    });
 
-    // api.interceptors.request.use(
-    //     config => {
-    //         const token = localStorage.getItem('token')
-    //         if (token) {
-    //             config.headers.Authorization = `Bearer ${token}`
-    //         }
-    //         return config
-    //     },
-    //     error => Promise.reject(error)
-    // )
-
-    return api
+    return api;
 }
 
 export default baseApi
