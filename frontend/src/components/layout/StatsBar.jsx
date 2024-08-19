de9cf4227096f0f10a209ee71454e2756c7a10fc
paramvir12121 <paramvir12121@gmail.com>
2024-08-19 00:30:25 -0400

    minor changes, db changed

--- frontend/src/components/layout/StatsBar.jsx
+++ frontend/src/components/layout/StatsBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Nav, Navbar } from 'react-bootstrap';
+import { Container, Row, Col, Nav} from 'react-bootstrap';
 
 
 const StatsBar = () => {
