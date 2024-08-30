fbeb3b0a17e74f0737e061983d3581935e6c6206
Paramvir Singh <paramvir12121@gmail.com>
2024-08-30 21:13:31 +0000

    working on integration figma

---
+++ frontend/src/figma/cta/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export const Cta = () => {
+  return (
+    <div className="CTA">
+      <div className="group">
+        <div className="GIF" />
+        <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        <div className="title">Main big title here</div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/testComponents/Test1.jsx
+++ frontend/src/testComponents/Test1.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import KeyboardTrainer from './KeyboardTrainer';
 import ToastComponent from '../components/common/ToastComponent';
+import { TopBar } from '../figma/topbar/main';
+import { Cta } from '../figma/cta/main';
+
 
 const Test1 = () => {
 
@@ -9,7 +12,10 @@ const Test1 = () => {
 
   return (
     <div>
-      <ToastComponent toastTitle="Test1 Title" toastMessage="Test1 Message" />  
+      <ToastComponent toastTitle="Test1 Title" toastMessage="Test1 Message" /> 
+      <TopBar />
+      <Cta />
+
       
     </div>
   );
