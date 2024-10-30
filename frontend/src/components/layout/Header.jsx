8c6a268bae040ee2e2ed145bde6ca1eb506642ff
Paramvir Singh <paramvir12121@gmail.com>
2024-10-30 20:36:38 +0000

    better mobile format

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -6,9 +6,7 @@ import Cookies from 'js-cookie';
 import ROUTES from '../../Routes';
 import LogoutButton from '../../auth/LogoutButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faUserCircle } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faUserCircle, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 import logo from '/logo1.png';
 
 const Header = () => {
@@ -29,68 +27,60 @@ const Header = () => {
 
     return (
         <Navbar expand="sm" className="mb-3 shadow-sm">
-            <Container>
+            <Container className="d-flex align-items-center">
                 <Navbar.Brand as={Link} to={ROUTES.DASHBOARD}>
-                <img
+                    <img
                         src={logo}
                         alt="Keyboard Bilingual"
-                        style={{ width: '45px', height: '45px' }} // Adjust the size as needed
+                        style={{ width: '45px', height: '45px' }}
                     />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {signedIn && (
-                            <>
-                                {/* <Nav.Link as={Link} to={ROUTES.DASHBOARD} className="fw-semibold">Dashboard</Nav.Link> */}
-                            </>
-                        )}
-                    </Nav>
-                    <Nav className="align-items-center">
-                        {signedIn ? (
-                            <Dropdown align="end">
-                                <Dropdown.Toggle as={Nav.Link} id="user-dropdown" className="me-2">
-                                    <FontAwesomeIcon icon={faUserCircle} size="lg" />
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu className='custom-dropdown-menu'>
-                                    <Dropdown.ItemText><FontAwesomeIcon icon={faUser} className="me-2"/> {username}</Dropdown.ItemText>
-                                    <Dropdown.ItemText> <FontAwesomeIcon icon={faEnvelope} className="me-2" /> {Cookies.get('email')}</Dropdown.ItemText>
-                                    <Dropdown.Divider />
-                                    
-                                    <Dropdown.Item as={Link} to={ROUTES.PROFILE} className="text-dark">
-                                    <FontAwesomeIcon icon={faUserCircle} size="lg" className="me-2"/>
-                                        Profile
-                                    </Dropdown.Item>
-                                    <Dropdown.Item as={Link} to={ROUTES.SETTINGS} className="text-dark">
-                                        <FontAwesomeIcon icon={faCog} className="me-2" />
-                                        Settings
-                                    </Dropdown.Item>
-                                    <Dropdown.Divider />
-                                    <div className="d-flex justify-content-center">
+                <Nav className="ms-auto d-flex align-items-center">
+                    {signedIn ? (
+                        <Dropdown align="end">
+                            <Dropdown.Toggle as={Nav.Link} id="user-dropdown" className="me-2">
+                                <FontAwesomeIcon icon={faUserCircle} size="lg" />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu className='custom-dropdown-menu'>
+                                <Dropdown.ItemText>
+                                    <FontAwesomeIcon icon={faUser} className="me-2" /> {username}
+                                </Dropdown.ItemText>
+                                <Dropdown.ItemText>
+                                    <FontAwesomeIcon icon={faEnvelope} className="me-2" /> {Cookies.get('email')}
+                                </Dropdown.ItemText>
+                                <Dropdown.Divider />
+                                <Dropdown.Item as={Link} to={ROUTES.PROFILE} className="text-dark">
+                                    <FontAwesomeIcon icon={faUserCircle} size="lg" className="me-2" />
+                                    Profile
+                                </Dropdown.Item>
+                                <Dropdown.Item as={Link} to={ROUTES.SETTINGS} className="text-dark">
+                                    <FontAwesomeIcon icon={faCog} className="me-2" />
+                                    Settings
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <div className="d-flex justify-content-center">
                                     <LogoutButton />
-                                    </div>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        ) : (
-                            <>
-                                <Button
-                                    variant="outline-primary"
-                                    className="me-2 px-3"
-                                    onClick={() => navigate(ROUTES.LOGIN)}
-                                >
-                                    Login
-                                </Button>
-                                <Button
-                                    variant="primary"
-                                    className="px-3"
-                                    onClick={() => navigate(ROUTES.SIGNUP)}
-                                >
-                                    Signup
-                                </Button>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
+                                </div>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    ) : (
+                        <div className="d-flex">
+                            <Button
+                                variant="outline-primary"
+                                className="me-2"
+                                onClick={() => navigate(ROUTES.LOGIN)}
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                variant="primary"
+                                onClick={() => navigate(ROUTES.SIGNUP)}
+                            >
+                                Signup
+                            </Button>
+                        </div>
+                    )}
+                </Nav>
             </Container>
         </Navbar>
     );
