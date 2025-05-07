fa80bb90d4df235b8b4010eb9164012ec3579e39
paramvir12121 <paramvir12121@gmail.com>
2025-05-07 03:45:47 -0400

    installed npm dependency and terraform fmt

--- terraform/gcp-cloud-run/backend.tf
+++ terraform/gcp-cloud-run/backend.tf
@@ -16,7 +16,7 @@ resource "google_cloud_run_service" "backend_service" {
     # Depend on the new secret-level IAM binding
     google_secret_manager_secret_iam_member.secret_accessor_binding
   ]
-  
+
   name     = "backend-service"
   location = var.region
 
@@ -24,13 +24,13 @@ resource "google_cloud_run_service" "backend_service" {
     spec {
       # Explicitly specify the service account to use
       service_account_name = "${var.project_number}-compute@developer.gserviceaccount.com"
-      
+
       containers {
         image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.artifact_repo}/backend_image:latest"
         # Define environment variables
         env {
           name  = "SUPABASE_URI_SECONDARY"
-          value = var.database_uri 
+          value = var.database_uri
         }
 
         # Add environment variable for API domain
@@ -38,9 +38,9 @@ resource "google_cloud_run_service" "backend_service" {
           name  = "API_DOMAIN"
           value = var.api_domain_name
         }
-        
-        
-        
+
+
+
         # Add environment variable to force HTTPS
         env {
           name  = "PREFERRED_URL_SCHEME"
@@ -50,21 +50,21 @@ resource "google_cloud_run_service" "backend_service" {
         ports {
           container_port = 5000
         }
-        
+
         resources {
           limits = {
             cpu    = "1"
             memory = "256Mi"
           }
         }
-        
+
         # Mount the secret as a volume
         volume_mounts {
           mount_path = "/secrets/my-secret"   # Path inside the container
           name       = var.secret_volume_name # Must match the volume name
         }
       }
-      
+
       # Define volumes block for secret mount
       volumes {
         name = var.secret_volume_name
@@ -112,23 +112,23 @@ resource "google_secret_manager_secret_iam_member" "secret_accessor_binding" {
 
 resource "google_cloud_run_service_revision" "backend_revision" {
   depends_on = [google_cloud_run_service.frontend_service]
-  
+
   service = google_cloud_run_service.backend_service.name
-  
+
   metadata {
     name = "${google_cloud_run_service.backend_service.name}-revision-${substr(md5(google_cloud_run_service.frontend_service.status[0].url), 0, 8)}"
   }
-  
+
   template {
     spec {
       # Explicitly specify the service account to use
       service_account_name = "${var.project_number}-compute@developer.gserviceaccount.com"
-      
+
       containers {
         image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.artifact_repo}/backend_image:latest"
         # Define environment variables
 
-         # Add the environment variable here
+        # Add the environment variable here
         env {
           name  = "ALLOWED_ORIGIN_PROD"
           value = google_cloud_run_service.frontend_service.status[0].url
@@ -136,7 +136,7 @@ resource "google_cloud_run_service_revision" "backend_revision" {
 
         env {
           name  = "SUPABASE_URI_SECONDARY"
-          value = var.database_uri 
+          value = var.database_uri
         }
 
         # Add environment variable for API domain
@@ -155,14 +155,14 @@ resource "google_cloud_run_service_revision" "backend_revision" {
         ports {
           container_port = 5000
         }
-        
+
         resources {
           limits = {
             cpu    = "1"
             memory = "256Mi"
           }
         }
-        
+
 
 
         # Mount the secret as a volume
@@ -171,7 +171,7 @@ resource "google_cloud_run_service_revision" "backend_revision" {
           name       = var.secret_volume_name # Must match the volume name
         }
       }
-      
+
 
       # Define volumes block for secret mount
       volumes {
@@ -212,11 +212,11 @@ resource "google_cloud_run_service_revision" "backend_revision" {
 #   ]
 #   location = var.region
 #   name     = var.api_domain_name
-  
+
 #   metadata {
 #     namespace = var.project_id
 #   }
-  
+
 #   spec {
 #     route_name = google_cloud_run_service.backend_service.name
 #   }
@@ -230,11 +230,11 @@ resource "google_cloud_run_service_revision" "backend_revision" {
 #   ]
 #   location = var.region
 #   name     = var.frontend_domain_name
-  
+
 #   metadata {
 #     namespace = var.project_id
 #   }
-  
+
 #   spec {
 #     route_name = google_cloud_run_service.frontend_service.name
 #   }
